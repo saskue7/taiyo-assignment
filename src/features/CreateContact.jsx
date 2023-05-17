c015ff6891345b6bb990de2e1f19b7085397da26
Saskue <narutoank2016@gmail.com>
2023-05-17 14:36:28 +0530

    13th

--- src/features/CreateContact.jsx
+++ src/features/CreateContact.jsx
@@ -27,9 +27,9 @@ const CreateContact = () => {
         <h1 className="text-center mb-8 font-bold text-2xl">Create Contact </h1>
         <form className="flex text-center md:flex-col items-center justify-center" >
           <label className="mr-1 ml-1">First Name</label>
-          <input onChange={firstNameChange} className="shadow appearance-none border border-blue-500 rounded w-xl py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline " maxlength="10" />
+          <input onChange={firstNameChange} type="text" className="shadow appearance-none border border-blue-500 rounded w-xl py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline " maxlength="10" />
           <label className="mr-1 ml-1">Last Name</label>
-          <input onChange={lastNameChange} className=" shadow appearance-none border border-blue-500 rounded w-xl py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" maxlength="10" />
+          <input onChange={lastNameChange} type="text" className=" shadow appearance-none border border-blue-500 rounded w-xl py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" maxlength="10" />
         </form>
         <button onClick={onSave} className="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded max-w-xl" >Save Contact</button>
       </div>
