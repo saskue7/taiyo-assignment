df48fc3a28a7d5fa3a34a09c16de49e4b6e66857
Saskue <narutoank2016@gmail.com>
2023-05-17 14:05:07 +0530

    8th time

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux/es/exports'
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root').render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-)
+))
