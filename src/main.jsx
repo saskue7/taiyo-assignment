1cdc67f4d91298e2be03351eb0586745f2f5bd89
Saskue <narutoank2016@gmail.com>
2023-05-17 13:55:59 +0530

    4th time

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.jsx'
 import './index.css'
-import { store } from './app/store.ts'
+import { store } from './app/store.js'
 import { Provider } from 'react-redux/es/exports'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
